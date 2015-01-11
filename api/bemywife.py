5abab08fe95e31ea18ae7263b100d7a5f563c8bd
Timo Djürken <timo.djuerken@gmail.com>
2015-01-10 22:04:11 -0800

    Added Cross-origin header to api

--- api/bemywife.py
+++ api/bemywife.py
@@ -2,9 +2,12 @@ import os
 import urlparse
 import logging
 
+from datetime import timedelta
+from functools import update_wrapper
+
 import psycopg2
 
-from flask import Flask, abort, jsonify, request
+from flask import Flask, abort, jsonify, request, make_response, current_app
 
 app = Flask(__name__)
 
@@ -29,11 +32,53 @@ def setup_logging():
         app.logger.addHandler(logging.StreamHandler())
         app.logger.setLevel(logging.DEBUG)
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 @app.route('/')
 def index():
     return 'It works! Be my wife?'
 
 @app.route('/login/<username>')
+@crossdomain(origin='*')
 def login(username):
     cur = pgconn.cursor()
     # Check if user id exists
@@ -64,6 +109,7 @@ def get_user_preferences(user_id):
     return preferences
 
 @app.route('/preferences/<int:user_id>', methods=['GET', 'POST'])
+@crossdomain(origin='*')
 def preferences(user_id):
     '''Return or edit preferences of the user'''
     cur = pgconn.cursor()
@@ -105,6 +151,7 @@ def preferences(user_id):
     abort(500)
 
 @app.route('/preferences/<int:user_id>/apply/<int:vin>')
+@crossdomain(origin='*')
 def apply_preferences(user_id, vin):
     '''Apply user preferences to target car'''
     return 'Not implemented yet.'
